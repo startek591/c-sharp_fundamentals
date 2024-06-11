074f3d76132d41934988216d6f243beb668b7090
jonathan_fambro <startek591@gmail.com>
2024-06-11 16:55:42 -0400

    Created a class, and  main method that prints Hello World

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        // Execution begins with the main function
+        // static means this function can run without
+        // creating an object
+        // void means that this function doesn't return a value
+        // args represent data that can be passed to our program
+        // from the command line as an array of strings
+        static void Main(string[] args)
+        {
+            // Prints Hello World on the console
+            Console.WriteLine("Hello World");
+        }
+    }
+
+}
