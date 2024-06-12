ee3376a914274d83e66ebc8b97ca64e19ee479a6
jonathan_fambro <startek591@gmail.com>
2024-06-12 14:02:55 -0400

    Do-While and While ConditionalLoops

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -335,6 +335,57 @@ namespace HelloWorld
             {
                 Console.WriteLine("Names are Equal");
             }
+
+            // While Loop
+            // You use the while loop when you want to execute
+            // as long as a condition is true
+
+            // This while loop will print odd numbers between
+            // 1 and 10
+            int i = 1;
+            while (i <= 10)
+            {
+                // % (Modulus) returns the remainder of a
+                // division. If it returns 0 that means the
+                // value is even
+                if (i % 2 == 0)
+                {
+                    i++;
+
+                    // Continue skips the rest of the code and
+                    // starts execution back at the top of the
+                    // while
+                    continue;
+                }
+
+                // Breaks jumps completely out of the loop
+                if (i == 9) break;
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // Do While Loop
+            // at least once
+
+            // Generate a random number
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num: ", secretNumber);
+
+            do
+            {
+                Console.Write("Enter a number between 1 & 10: ");
+
+                // Use Convert to switch the string into an int
+                // Other Convert options: ToBoolean, ToByte,
+                // ToChar, ToDecimal, ToDouble, ToInt64
+                // ToString, and they can convert from any
+                // type to any other type
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it was {0}", secretNumber);
         }
     }
 
