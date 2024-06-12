40adced215a1e374d3d4a843001c363e65c16602
jonathan_fambro <startek591@gmail.com>
2024-06-12 13:18:06 -0400

    Arrays and it's methods

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -7,6 +7,14 @@ namespace HelloWorld
 {
     public class Program
     {
+        // ------ FUNCTIONS -------
+        static void PrintArray(int[] intArray, string mess)
+        {
+            foreach (int k in intArray)
+            {
+                Console.WriteLine("{0} : {1} ", mess, k);
+            }
+        }
         // Execution begins with the main function
         // static means this function can run without
         // creating an object
@@ -155,6 +163,115 @@ namespace HelloWorld
 
             // Verbatim strings ignore escape characters
             Console.WriteLine(@"Exactly What I Typed\n");
+
+            // Arrays
+            // Arrays are just boxes inside of a bigger box
+            // that can contain many values of the same
+            // data type
+            // Each item is assigned a key starting at 0
+            // and incrementing up from there
+
+            // Define an array which holds 3 values
+            // Arrays have fixed sizes
+            int[] favNums = new int[3];
+
+            // Add a value to the array
+            favNums[0] = 23;
+
+            // Retrieve a value
+            Console.WriteLine("favNum 0: {0}", favNums[0]);
+
+            // Create an fill array
+            string[] customers = { "Bob", "Sally", "Sue" };
+
+            // You can use var to create arrays, but the
+            // values must be of the same type
+            var employees = new[] { "Mike", "Paul", "Rick" };
+
+            // Create an array of base objects which is the
+            // base type of all other types
+            object[] randomArray = { "Paul", 45, 1.234 };
+
+            // GetType knows its true type
+            Console.WriteLine("randomArray 0: {0}", randomArray[0].GetType());
+
+            // Get number of items in array
+            Console.WriteLine("Array Size: {0}", randomArray.Length);
+
+            // Use for loop to cycle through the array
+            for (int j = 0; j < randomArray.Length; j++)
+            {
+                Console.WriteLine("Array {0}: Value: {1}", j, randomArray[j]);
+            }
+
+            // Multidimensional arrays
+            // When you define an array like arrName[5] you
+            // are saying you want to create boxes stacked
+            // vertically
+            string[,] custNames = new string[2, 2] { { "Bob", "Smith", }, { "Sally", "Smith" } };
+
+            // Get value in MD array
+            Console.WriteLine("MD Value: {0}", custNames.GetValue(1, 1));
+
+            // Cycle through the multidimensional array
+            // Get length of multidimensional array column
+            for (int j = 0; j < custNames.GetLength(0); j++)
+            {
+                // Get length of multidimensional array column
+                for (int k = 0; k < custNames.GetLength(1); k++)
+                {
+                    Console.Write("{0} ", custNames[j, k]);
+                }
+                Console.WriteLine();
+            }
+
+            // An array like arrName[2,2,3] would be like a
+            // stack of 3 spread sheets with 2 rows and 2 columns
+            // columns worth of data on each page
+
+            // foreach can be used to cycle through an array
+            int[] randNums = { 1, 4, 9, 2 };
+
+            // You can pass an array to a function
+            PrintArray(randNums, "ForEach");
+
+            // Sort array
+            Array.Sort(randNums);
+
+            // Reverse array
+            Array.Reverse(randNums);
+
+            // Get index of match or return -1
+            Console.WriteLine("1 at index: {0}", Array.IndexOf(randNums, 1));
+
+            // Change value at index 1 to 0
+            randNums.SetValue(0, 1);
+
+            // Copy part of an array to another
+            int[] srcArray = { 1, 2, 3 };
+            int[] destArray = new int[2];
+            int startInd = 0;
+            int length = 2;
+
+            Array.Copy(srcArray, startInd, destArray, startInd, length);
+
+            PrintArray(destArray, "Copy");
+
+            // Create an array with CreateInstance
+            Array anotherArray = Array.CreateInstance(typeof(int), 10);
+
+            // Copy values in srcArray to destArray starting
+            // at index 5 in destination
+            srcArray.CopyTo(anotherArray, 5);
+
+            foreach (int m in anotherArray)
+            {
+                Console.WriteLine("CopyTo: {0}", m);
+            }
+
+
+
+
 
 
 
