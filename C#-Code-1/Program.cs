2ad313b1fbd375f148c269a24ad5c69c0bf2efdb
jonathan_fambro <startek591@gmail.com>
2024-06-11 21:40:28 -0400

    Format strings

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -93,6 +93,18 @@ namespace HelloWorld
             int intNum = 10;
             long longNum = intNum;
 
+            // Format output for currency
+            Console.WriteLine("Currency: {0:c}", 23.455);
+
+            // Pad with zeroes
+            Console.WriteLine("Pad with 0s: {0:d4}", 23);
+
+            // Define decimals
+            Console.WriteLine("3 Decimals: {0:f3}", 23.4555);
+
+            // Add commas and decimals
+            Console.WriteLine("Commas: {0:n4}", 2300);
+
 
 
 
