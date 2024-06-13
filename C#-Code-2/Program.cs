acb1c99a6eaed1a9fb3ab3da726dcd461cc0021f
jonathan_fambro <startek591@gmail.com>
2024-06-13 10:12:08 -0400

    Created a struct class that within the Program class and use both length and width fields

--- C#-Code-2/Program.cs
+++ C#-Code-2/Program.cs
@@ -8,7 +8,37 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Created New Application");
+            // Create a Rectangle
+            Rectangle rect1;
+
+            // Add values to it and run the Area method
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1: {0}", rect1.Area());
+
+        }
+
+        // Structs
+        // A struct is a user defined type that
+        // contains multiple fields and methods
+        struct Rectangle
+        {
+            public double length;
+
+            public double width;
+
+            // You can create a constructor method 
+            // that will set the values for fields
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
         }
     }
 }
