5b6d1e7b020c44caf9183e94494d8238b6480405
jonathan_fambro <startek591@gmail.com>
2024-06-13 14:18:29 -0400

    Created Animal class and use getter and setters to retrieve both name and sound

---
+++ C#-Code-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// We cover public, private, protected, constants
+// read-only fields, constructors, setters, getters
+// properties and more on static
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+
+            // Cal the setter
+            cat.SetName("Whiskers");
+
+            // Set the property
+            cat.Sound = "Meow";
+
+            Console.WriteLine("This cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+
+            Console.ReadLine();
+
+        }
+    }
+}
