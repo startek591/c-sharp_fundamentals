3c97238b9e9a98a822f449f4d192c5cadaab7d81
jonathan_fambro <startek591@gmail.com>
2024-07-03 14:04:11 -0400

    Data Type Conversions

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -76,6 +76,40 @@ namespace HelloWorld
             float fltPiVal = 3.141592F;
             float fltBigNum = 3.000002F;
             Console.WriteLine("FLT : PT + bigNum = {0}", fltPiVal + fltBigNum);
+
+            // Data Type Conversion
+            // You can convert from string to other types with Parse
+            bool boolFromStr = bool.Parse("True");
+            int intFromStr = int.Parse("100");
+            double dblFromStr = double.Parse("1.234");
+
+            // Convert double into a string
+            string strVal = dblFromStr.ToString();
+
+            // Get the new data type
+            Console.WriteLine($"Data type : {strVal.GetType()}");
+
+            // Cast double into integer (Explicit Conversion)
+            // Put the data type to convert into between ()
+            double dblNum = 12.345;
+            Console.WriteLine($"Integer : {(int)dblNum}");
+
+            // Cast integer into long (Implicit Conversion)
+            // smaller size type to a larger size
+            int intNum = 10;
+            long longNum = intNum;
+
+            // Format output for currency
+            Console.WriteLine("Currency : {0:c}", 23.455);
+            // Pad with zeroes
+            Console.WriteLine("Pad with 0s : {0:d4}", 23);
+
+            // Define decimals
+            Console.WriteLine("3 Decimals : {0:f3}", 23.4555);
+
+            // Add commas and decimals
+            Console.WriteLine("Commas : {0:n4}", 2300);
+
         }
     }
 }
