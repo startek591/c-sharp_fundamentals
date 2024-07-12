6df271d95bca0876909788875b3c016fb4162d57
jonathan_fambro <startek591@gmail.com>
2024-07-12 16:47:03 -0400

    Observable and Observer

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace RxSamples
+﻿using System;
+
+namespace RxSamples
 {
+    public class Market : IObservable<float>
+    {
+        public IDisposable Subscribe(IObserver<float> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program : IObserver<float>
     {
+        public Program()
+        {
+            var market = new Market();
+            market.Subscribe(this);
+        }
         static void Main(string[] args)
         {
             // OnNext* --> (OnError | OnCompleted)?
