24ae901d87e467687c9dc51aad0ce542d657f28a
jonathan_fambro <startek591@gmail.com>
2024-07-12 14:12:00 -0400

    Observer Concept - IObserver<T>

---
+++ RxSamples1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RxSamples
+{
+    class Program : IObserver<float>
+    {
+        static void Main(string[] args)
+        {
+            // OnNext* --> (OnError | OnCompleted)?
+        }
+
+        public void OnNext(float value)
+        {
+            Console.WriteLine($"Market gave us {value}");
+        }
+
+        public void OnError(Exception error)
+        {
+
+        }
+
+        public void OnCompleted()
+        {
+
+        }
+    }
+}
