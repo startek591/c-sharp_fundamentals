d5c1c48c34d4ea5a6d72c5f9e8a8c61cf8578b76
jonathan_fambro <startek591@gmail.com>
2024-07-12 17:12:24 -0400

    Subject without the Observer and Observable

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -3,44 +3,25 @@ using System.Reactive.Subjects;
 
 namespace RxSamples
 {
-    public class Market : IObservable<float>
-    {
-        public IDisposable Subscribe(IObserver<float> observer)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    class Program : IObserver<float>
+    class Program
     {
         public Program()
         {
             var market = new Subject<float>();
-            market.Subscribe(this);
+            market.Subscribe(
+                f => Console.WriteLine($"Price is {f}"),
+                () => Console.WriteLine("Sequence is complete")
+            );
 
             market.OnNext(1.24f);
-            market.OnError(new Exception("opps"));
-            // market.OnCompleted();
+            // market.OnError(new Exception("oops"));
+            market.OnCompleted();
         }
         static void Main(string[] args)
         {
             // OnNext* --> (OnError | OnCompleted)?
 
             new Program();
-        }
-
-        public void OnNext(float value)
-        {
-            Console.WriteLine($"Market gave us {value}");
-        }
-
-        public void OnError(Exception error)
-        {
-            Console.WriteLine($"We got an error {error.Message}");
-        }
-
-        public void OnCompleted()
-        {
-            Console.WriteLine("Sequence is complete");
         }
     }
 }
