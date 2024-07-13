6f4e4c35fefa43a22f8450c5ed48d86caf6c544e
jonathan_fambro <startek591@gmail.com>
2024-07-13 13:40:55 -0400

    Sequence Generators

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -10,65 +10,26 @@ namespace RxSamples
 {
     class Program
     {
-        private static IObservable<string> Blocking()
-        {
-            var subj = new ReplaySubject<string>();
-            subj.OnNext("foo");
-            subj.OnNext("bar");
-            subj.OnCompleted();
-            Thread.Sleep(3000);
-            return subj;
-        }
-
-        private static IObservable<string> NonBlocking()
-        {
-            return Observable.Create<string>(obeserver =>
-            {
-                obeserver.OnNext("foo");
-                obeserver.OnNext("bar");
-                obeserver.OnCompleted();
-                Thread.Sleep(3000);
-                return Disposable.Empty;
-            });
-        }
-
-        public static IObservable<T> Return<T>(T value)
-        {
-            return Observable.Create<T>(x =>
-            {
-                x.OnNext(value);
-                x.OnCompleted();
-                return Disposable.Empty;
-            });
-        }
         static void Main(string[] args)
         {
-            var obs = Observable.Create<string>(o =>
-            {
-                var timer = new Timer(1000);
-                timer.Elapsed += (sender, e) => o.OnNext($"tick {e.SignalTime}");
-                timer.Elapsed += TimerOnElapsed;
-                timer.Start();
-                return () =>
-                {
-                    timer.Elapsed -= TimerOnElapsed;
-                    timer.Dispose();
-                };
-            });
+            var observable = Observable.Generate(
+              0,
+              x => x < 10,
+              x => x + 1,
+              x => x,
+              x => TimeSpan.FromSeconds(x)
+            );
 
-            obs.Inspect("timer");
-            Console.ReadLine();
+            var subscription = observable.Subscribe(
+              onNext: x => Console.WriteLine($"Received {x}"),
+              onError: ex => Console.WriteLine($"Error: {ex}"),
+              onCompleted: () => Console.WriteLine("Completed")
+            );
 
-            var sub = obs.Inspect("timer");
-            Console.ReadLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            subscription.Dispose();
 
-            sub.Dispose();
-            Console.ReadLine();
-        }
-
-        private static void TimerOnElapsed(object sender1, ElapsedEventArgs args)
-        {
-            Console.WriteLine($"tock {args.SignalTime}");
         }
     }
 }
