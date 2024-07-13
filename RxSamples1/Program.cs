1412eb78f263d698100e89b3e1a3eff99306e05f
jonathan_fambro <startek591@gmail.com>
2024-07-13 13:19:59 -0400

    More Complex Observable.Create Scenario

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Immutable;
 using System.Reactive.Subjects;
 using System.Reactive.Linq;
 using System.Reactive.Disposables;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace RxSamples
 {
@@ -29,10 +31,44 @@ namespace RxSamples
                 return Disposable.Empty;
             });
         }
+
+        public static IObservable<T> Return<T>(T value)
+        {
+            return Observable.Create<T>(x =>
+            {
+                x.OnNext(value);
+                x.OnCompleted();
+                return Disposable.Empty;
+            });
+        }
         static void Main(string[] args)
         {
-            Blocking().Inspect("blocking");
-            NonBlocking().Inspect("nonblocking");
+            var obs = Observable.Create<string>(o =>
+            {
+                var timer = new Timer(1000);
+                timer.Elapsed += (sender, e) => o.OnNext($"tick {e.SignalTime}");
+                timer.Elapsed += TimerOnElapsed;
+                timer.Start();
+                return () =>
+                {
+                    timer.Elapsed -= TimerOnElapsed;
+                    timer.Dispose();
+                };
+            });
+
+            obs.Inspect("timer");
+            Console.ReadLine();
+
+            var sub = obs.Inspect("timer");
+            Console.ReadLine();
+
+            sub.Dispose();
+            Console.ReadLine();
+        }
+
+        private static void TimerOnElapsed(object sender1, ElapsedEventArgs args)
+        {
+            Console.WriteLine($"tock {args.SignalTime}");
         }
     }
 }
