c5675690509bb49619bb299be011eaf4005cb429
jonathan_fambro <startek591@gmail.com>
2024-07-13 12:31:24 -0400

    Simple Factory Methods

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -1,39 +1,16 @@
 ﻿using System;
 using System.Collections.Immutable;
 using System.Reactive.Subjects;
-using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace RxSamples
 {
-    public class Market : IObservable<float>
-    {
-        private ImmutableHashSet<IObserver<float>> observers = ImmutableHashSet<IObserver<float>>.Empty;
-        public IDisposable Subscribe(IObserver<float> observer)
-        {
-            observers = observers.Add(observer);
-            return Disposable.Create(() =>
-            {
-                observers = observers.Remove(observer);
-            });
-        }
-
-        public void Publish(float price)
-        {
-            foreach (var o in observers)
-                o.OnNext(price);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var market = new Market();
-            var sub = market.Inspect("market");
-            market.Publish(123.4f);
-
-            sub.Dispose();
-
-            market.Publish(321.0f);
+            var obs = Observable.Return(42); // ReplaySubject
+            obs.Inspect("obs");
         }
     }
 }
