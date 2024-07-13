164be32d55a215938a9c9c4cd75da68ad80de6bb
jonathan_fambro <startek591@gmail.com>
2024-07-13 11:39:24 -0400

    AsyncSubject Example

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -3,22 +3,17 @@ using System.Reactive.Subjects;
 
 namespace RxSamples
 {
-    public class Scada
-    {
-        private BehaviorSubject<double> sensorValue;
-        public IObservable<double> SensorValue => sensorValue;
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var sensorReading = new BehaviorSubject<double>(-1.0);
+            var sensor = new AsyncSubject<double>();
+            sensor.Inspect("async");
 
-            sensorReading.Inspect("sensor");
-
-            sensorReading.OnNext(0.99);
-
-            sensorReading.OnCompleted();
+            sensor.OnNext(1.0);
+            sensor.OnNext(2.0);
+            sensor.OnNext(3.0);
+            sensor.OnCompleted();
         }
     }
 }
