98ddc6957258f103dd98d0bb4c3a5aee1150fd8f
jonathan_fambro <startek591@gmail.com>
2024-07-13 11:53:56 -0400

    Implementing IObservable

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Reactive.Subjects;
+using System.Reactive.Disposables;
 
 namespace RxSamples
 {
+    public class Market : IObservable<float>
+    {
+        private ImmutableHashSet<IObserver<float>> observers = ImmutableHashSet<IObserver<float>>.Empty;
+        public IDisposable Subscribe(IObserver<float> observer)
+        {
+            observers = observers.Add(observer);
+            return Disposable.Create(() =>
+            {
+                observers = observers.Remove(observer);
+            });
+        }
+
+        public void Publish(float price)
+        {
+            foreach (var o in observers)
+                o.OnNext(price);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var sensor = new AsyncSubject<double>();
-            sensor.Inspect("async");
+            var market = new Market();
+            var sub = market.Inspect("market");
+            market.Publish(123.4f);
 
-            sensor.OnNext(1.0);
-            sensor.OnNext(2.0);
-            sensor.OnNext(3.0);
-            sensor.OnCompleted();
+            sub.Dispose();
+
+            market.Publish(321.0f);
         }
     }
 }
