85750f29a8ccf5646c996727bcff6817f0110db4
jonathan_fambro <startek591@gmail.com>
2024-07-13 12:51:47 -0400

    Observable.Create - Blocking and NonBlocking

--- RxSamples1/Program.cs
+++ RxSamples1/Program.cs
@@ -2,15 +2,37 @@
 using System.Collections.Immutable;
 using System.Reactive.Subjects;
 using System.Reactive.Linq;
+using System.Reactive.Disposables;
 
 namespace RxSamples
 {
     class Program
     {
+        private static IObservable<string> Blocking()
+        {
+            var subj = new ReplaySubject<string>();
+            subj.OnNext("foo");
+            subj.OnNext("bar");
+            subj.OnCompleted();
+            Thread.Sleep(3000);
+            return subj;
+        }
+
+        private static IObservable<string> NonBlocking()
+        {
+            return Observable.Create<string>(obeserver =>
+            {
+                obeserver.OnNext("foo");
+                obeserver.OnNext("bar");
+                obeserver.OnCompleted();
+                Thread.Sleep(3000);
+                return Disposable.Empty;
+            });
+        }
         static void Main(string[] args)
         {
-            var obs = Observable.Throw<int>(new Exception("oops")); // ReplaySubject
-            obs.Inspect("obs");
+            Blocking().Inspect("blocking");
+            NonBlocking().Inspect("nonblocking");
         }
     }
 }
