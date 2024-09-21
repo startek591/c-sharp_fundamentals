4deb1ca43d106c80f6010a89a0ae2e69b56cdad5
jonathan_fambro <startek591@gmail.com>
2024-09-21 11:48:09 -0400

    Console Methods

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -143,6 +143,26 @@ namespace HelloWorld
         {
             // Print Hello World on the console
             Console.WriteLine("Hello World!");
+
+            // ---- CONSOLE METHODS ----
+            // Change the text color in the console
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            // Change background color
+            Console.BackgroundColor = ConsoleColor.White;
+
+            // Set background for whole console
+            Console.Clear();
+
+            // Outputs text without a newline
+            Console.Write("What is your name?");
+
+            // Stores data entered by user in name
+            string? name = Console.ReadLine();
+
+            // Outputs Hello + values stored in name
+            Console.WriteLine($"Hello {name}");
         }
+
     }
 }
