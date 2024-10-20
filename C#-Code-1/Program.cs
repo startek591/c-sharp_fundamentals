665efa8833a336e6677dda06f80a776ecb01bb29
jonathan_fambro <startek591@gmail.com>
2024-10-20 13:15:58 -0400

    Data Type Conversion

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -93,6 +93,28 @@ namespace HelloWorld
             // uint : 32-bit unsigned int 0 to 4,294,967,295
             // ulong : 64-bit unsigned int 0 to 18,446,744,073,709,551,615
             // ushort : 16-bit unsigned int 0 to 65,535
+
+            // ---- DATA TYPE CONVERSION
+            // You can convert from string to other types with Parse
+            bool boolFromStr = bool.Parse("True");
+            int intFromStr = int.Parse("100");
+            double dblFromStr = double.Parse("1.234");
+
+            // Convert double into a string
+            string strVal = dblFromStr.ToString();
+
+            // Get the new data type
+            Console.WriteLine($"Data type : {strVal.GetType()}");
+
+            // Cast double into integer (Explicit Conversion) 
+            // Put the data type to convert into between ()
+            double dblNum = 12.345;
+            Console.WriteLine($"Ingeter : {(int)dblNum}");
+
+            // Cast integer into long (Implicit Conversion)
+            // smaller size type to a larger size
+            int intNum = 10;
+            long longNum = intNum;
         }
     }
 }
