c4487680c9bd06b1c5a530ad08c7cb7204c36462
jonathan_fambro <startek591@gmail.com>
2024-10-21 16:51:34 -0400

    Function - Pass by reference

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -63,7 +63,16 @@ namespace HelloWorld
         {
             solution = x * 2;
         }
-
+        // --- PASS BY REFERENCE
+        // If a variable is passed by reference changes
+        // to its value in the method effect it outside
+        // of the method
+        public static void Swap(ref int num3, ref int num4)
+        {
+            int temp = num3;
+            num3 = num4;
+            num4 = temp;
+        }
 
 
         // Execution begins in the main function
@@ -601,6 +610,17 @@ namespace HelloWorld
             DoubleIt(15, out solution);
 
             Console.WriteLine("15 * 2 = {0}", solution);
+
+            // ---- PASS BY REFERENCE
+            int num3 = 10;
+            int num4 = 20;
+
+            Console.WriteLine("Before Swap num1 : {0} num2 : {1}", num3, num4);
+
+            Swap(ref num3, ref num4);
+
+            Console.WriteLine("After Swap num1 : {0} num2 : {1}", num3, num4);
+
         }
     }
 }
