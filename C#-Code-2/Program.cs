8d4add36148d5af4d474eb869a70da685abe1a4a
jonathan_fambro <startek591@gmail.com>
2024-10-23 14:29:20 -0400

    Creating a struct class, rectange, add values and using a constructor

--- C#-Code-2/Program.cs
+++ C#-Code-2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp2;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Reactangle
+            Reactangle rect1;
+
+            // Add values to it and run the Area method
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
+
+            // Use a constructor to create a Reactangle
+            Reactangle rect2 = new Reactangle(100, 40);
+
+        }
+        // ---- STRUCTS 
+        // A struct is a user defined type that 
+        // contain multiple fields and methods
+        struct Reactangle
+        {
+            public double length;
+            public double width;
+
+            // You can create a constructor method
+            // that will set the values for fields
+            public Reactangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+    }
+}
