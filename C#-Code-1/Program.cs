1b9d70248602b498edbb832f70ca4eb567867492
jonathan_fambro <startek591@gmail.com>
2025-05-19 10:44:58 -0400

    Array, Conditional Statements, Switch, While loops, Do While, Exception Handling, and String Builder

--- C#-Code-1/Program.cs
+++ C#-Code-1/Program.cs
@@ -14,6 +14,28 @@ namespace HelloWorld
     // Our class name is Program
     public class Program
     {
+
+        // --- FUNCTIONS ---
+        static void PrintArray(int[] intArray, string mess)
+        {
+            foreach (int k in intArray)
+            {
+                Console.WriteLine("{0} : {1} ", mess, k);
+            }
+        }
+
+        static double DoDivision(double x, double y)
+        {
+            if (y == 0)
+            {
+                // We are throwing an exeception because
+                // you can't divide by zero
+                throw new System.DivideByZeroException();
+            }
+            return x / y;
+        }
+
+
         // Execution begins in the main function
         // static means this function can run without
         // creating an object
@@ -248,6 +270,265 @@ namespace HelloWorld
                 }
                 Console.WriteLine();
             }
+
+            // An array like arrName[2,2,3] would be like a 
+            // stack of 3 spread sheets  with 2 rows and 2 columns
+            // columns worth of data on each page
+
+            // foreach can be used to cycle through an array
+            int[] randNums = { 1, 2, 3, 4 };
+
+            // You can pass an array to a function
+            PrintArray(randNums, "Foreach");
+
+            // Sort array
+            Array.Sort(randNums);
+
+            // Reverse array
+            Array.Reverse(randNums);
+
+            // Get index of match or return -1
+            Console.WriteLine("1 at index : {0}", Array.IndexOf(randNums, 1));
+
+            // Change value at index 1 to 0
+            randNums.SetValue(0, 1);
+
+            // Copy part of an array to another
+            int[] srcArray = { 1, 2, 3 };
+            int[] destArray = new int[2];
+            int startInd = 0;
+            int length = 2;
+
+            Array.Copy(srcArray, startInd, destArray, startInd, length);
+
+            PrintArray(destArray, "Copy");
+
+            // Create an array with CreateInstance
+            Array anotherArray = Array.CreateInstance(typeof(int), 10);
+
+
+            // Copy values in srcArray to destArray starting
+            // at index 5 in destination
+            srcArray.CopyTo(anotherArray, 5);
+
+            foreach (int m in anotherArray)
+            {
+                Console.WriteLine("CopyTo : {0}", m);
+            }
+
+            // ---- IF / ELSE / -----
+            // Relational Operators : > < >= <= == !=
+            // Logical Operators : && || !
+
+            int age = 17;
+
+            if ((age >= 5) && (age <= 7))
+            {
+                Console.WriteLine("Go to elementary school");
+            }
+            else if ((age > 7) && (age < 13))
+            {
+                Console.WriteLine("Go to middle school");
+            }
+            else if ((age > 13) && (age < 19))
+            {
+                Console.WriteLine("Go to high school");
+            }
+            else
+            {
+                Console.WriteLine("Go to college");
+            }
+
+            if ((age < 14) || (age > 67))
+            {
+                Console.WriteLine("You shouldn't work");
+            }
+
+            Console.WriteLine("! true = " + (!true));
+
+            // Ternary Operator
+            // Assigns the 1st value if true and otherwise
+            // the 2nd
+            bool canDrive = age >= 16 ? true : false;
+
+            // Switch is used when you have limited options
+            // The only way to use ranges is to stack
+            // the possible values
+            switch (age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to Day Care");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to Preschool");
+                    break;
+                case 5:
+                    Console.WriteLine("Go to Kindergarden");
+                    break;
+                default:
+                    Console.WriteLine("Go to another school");
+                    // You can also jump out of a switch
+                    // with goto
+                    goto OtherSchool;
+
+            }
+
+        OtherSchool:
+            Console.WriteLine("Elementary, Middle, High School");
+
+            // To compare strings use Equals
+            string name2 = "Derek";
+            string name3 = "Derek";
+
+            if (name2.Equals(name3, StringComparison.Ordinal))
+            {
+                Console.WriteLine("Names are Equal");
+            }
+
+            // ---- WHILE LOOPS
+            // You use the while loop when you want to execute
+            // as long as a condition is true
+
+            // This while loop will print odd numbers between
+            // 1 and 10
+            int i = 1;
+            while (i <= 10)
+            {
+                // % (Modulus) returns the remainder of a
+                // division. If it returns 0 that means the
+                // value is even
+                if (i % 2 == 0)
+                {
+                    i++;
+
+                    // Continue skip the rest of the code and 
+                    // starts execution back at the top of the 
+                    // while
+                    continue;
+                }
+
+                // Break jumps completely out of the loop 
+                if (i == 9) break;
+
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // ---- DO WHILE LOOP ---
+            // Use do while when you must execute the code
+            // at least code
+
+            // Generate a random number
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num: ", secretNumber);
+
+            do
+            {
+                Console.Write("Enter a number between 1 & 10 : ");
+
+                // Use Convert to switch the string into an int
+                // Other Convert options: ToBoolean, ToByte,
+                // ToChar, ToDecimal, ToDouble, ToInt64,
+                // ToString, and they can convert from any
+                // type to any other type
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it is was {0}", secretNumber);
+
+            // ---- EXCEPTION HANDLING ----
+            // We use exception handling to catch errors
+            // that could crash our program
+            double num1 = 5;
+            double num2 = 0;
+
+            // Code that could cause an error is surrounded 
+            // by a try block
+            try
+            {
+                Console.WriteLine("5 / 0 = {0}", DoDivision(num1, num2));
+            }
+
+            // We catch the error and warn the user
+            // rather then crash the program
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can't Divide by Zero");
+
+                // Get additional info on the exception
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            // This is the default catch all for exceptions
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            // finally always runs provides for clean up
+            finally
+            {
+                Console.WriteLine("Cleaning Up");
+            }
+
+            // ---- STRINGBUILDER ----
+            // Each time you change a string you are actually
+            // creating a new string which is inefficient
+            // when you are working with large blocks of text
+            // StringBuilders actually change the text
+            // rather then make a copy
+
+            // Create a StringBuilder with a default size
+            // of 16 characters, but it grows automatically
+            StringBuilder sb = new StringBuilder("Random Text");
+
+            // Create a StringBuilder with a size of 256
+            StringBuilder sb2 = new StringBuilder("More Stuff that is very important", 256);
+
+            // Get max size
+            Console.WriteLine("Capacity : {0}", sb2.Capacity);
+
+            // Get length
+            Console.WriteLine("Length : {0}", sb2.Length);
+
+            // Add text to StringBuilder
+            sb2.AppendLine("\nMore important text");
+
+            // Define culture specific formatting
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+
+            // Append a format string
+            string bestCust = "Bob Smith";
+            sb2.AppendFormat(enUS, "Best Customer : {0}", bestCust);
+
+            // Output StringBuilder
+            Console.WriteLine(sb2.ToString());
+
+            // Clear a string builder
+            sb2.Clear();
+
+            sb2.Append("Random Text");
+
+            // Are objects equal
+            Console.WriteLine(sb.Equals(sb2));
+
+            // Insert at an index
+            sb2.Insert(11, " that's Great");
+
+            Console.WriteLine("Insert : {0}", sb2.ToString());
+
+            // Remove number of characters starting at index
+            sb2.Remove(11, 7);
+
+            Console.WriteLine("Remove : {0}", sb2.ToString());
+
         }
     }
 }
